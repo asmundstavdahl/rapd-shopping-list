a98edcda1006b862e13f83bfd9d173c0102c208b
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2018-09-05 13:38:43 +0200

    add Entity->findAllBelonging()

--- src/Entity.php
+++ src/Entity.php
@@ -2,4 +2,11 @@
 
 class Entity extends \Rapd\PersistableEntity {
 	use \Rapd\Prototype;
+
+	function findAllBelonging(string $entityClass) : array {
+		$myTable = self::getTable();
+		return $entityClass::findAllWhere("{$myTable}_id = :id", [
+			":id" => $this->id
+		]);
+	}
 }
