be17e65497b40bb9adda339ddde7f109c69c107a
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2018-09-07 10:37:57 +0200

    add Entity->findAllRelatedVia for MtM relations

--- src/Entity.php
+++ src/Entity.php
@@ -18,13 +18,50 @@ class Entity extends \Rapd\PersistableEntity {
 	 * 
 	 * EntityA belongs to EntityB if entity_a.entity_b_id = entity_b.id.
 	 * 
-	 * @example $entityB->findAllBelonging(EntityA::class)
+	 * @usage $entityB->findAllBelonging(EntityA::class)
 	 */
 	function findAllBelonging(string $entityClass) : array {
 		$myTable = self::getTable();
 		return $entityClass::findAllWhere("{$myTable}_id = :id", [
 			":id" => $this->id
 		]);
+	}
+
+	/**
+	 * Find all related entities via specified many-to-many entity class.
+	 *
+	 * EntityA is related to EntityB via MtM if there is an MtM record where
+	 * mtm.entity_a_id = entity_a.id AND mtm.entity_b_id = entity_b.id.
+	 *
+	 * @usage $entityA->findAllRelatedVia(EntityB::class, AhasB::class)
+	 */
+	function findAllRelatedVia(string $entityClass, string $mtmEntityClass) : array {
+		Database::assertInitialized();
+
+		$myTable = self::getTable();
+		$theirTable = $entityClass::getTable();
+		$mtmTable = $mtmEntityClass::getTable();
+
+		$sql = "SELECT * FROM `{$mtmTable}`
+			LEFT JOIN `{$theirTable}`
+			ON `{$mtmTable}`.`{$theirTable}_id` = `{$theirTable}`.id
+			WHERE `{$mtmTable}`.`{$myTable}_id` = :id";
+		$stmt = Database::$pdo->prepare($sql);
+		$stmt->execute([":id" => $this->id]);
+
+		$rows = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		$entities = [];
+		foreach($rows as $values){
+			$entity = new $entityClass();
+			foreach($values as $field => $value){
+				if(array_key_exists($field, $entityClass::$fields)){
+					$entity->{$field} = $value;
+				}
+			}
+			$entities[] = $entity;
+		}
+
+		return $entities;
 	}
 
 	/**
