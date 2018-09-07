c17a888faa9518f19c52bec6eccad382b486678c
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2018-09-07 11:17:53 +0200

    add 400 Bad Request response

--- src/Respond.php
+++ src/Respond.php
@@ -4,6 +4,14 @@ use \Rapd\View;
 use \Rapd\Environment;
 
 class Respond {
+	static function badRequest(string $reason) : string {
+		Environment::set("TITLE", "404 Bad Request");
+		header("HTTP/1.1 400 Bad Request");
+		return View::render("400", [
+			"reason" => $reason
+		]);
+	}
+
 	static function forbidden(string $reason) : string {
 		Environment::set("TITLE", $reason);
 		header("HTTP/1.1 403 Forbidden");
---
+++ templates/400.php
@@ -0,0 +1,6 @@
+<?= render("header") ?>
+
+<h1>400 Bad Request</h1>
+<h2><?= $reason ?></h2>
+
+<?= render("footer") ?>
